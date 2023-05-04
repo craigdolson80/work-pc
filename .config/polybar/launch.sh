9b2c55f769060f1d09aaf0019977a1c9a3099d76
Craig Olson <craigdolson@gmail.com>
2023-05-04 08:31:23 -0500

    adding BSPWM and related configs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch left and right
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar right 2>&1 | tee -a /tmp/polybar2.log & disown
